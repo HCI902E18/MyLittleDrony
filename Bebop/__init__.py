2d184101136cf487e161dc658b739d0c67eea1d5
Kasper Helsted <khelst13@student.aau.dk>
2018-10-10 07:56:37 +0200

    Added inputz and pyparrot as egg instead of submodule

--- Bebop/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Simple example showing how to get gamepad events."""
-
-from controller.Controller import Controller
-
-c = Controller()
-
-if __name__ == "__main__":
-    c.start()
---
+++ app.py
@@ -0,0 +1,7 @@
+from inputz import Devices
+from pyparrot.Bebop import Bebop
+
+d = Devices()
+bebop = Bebop()
+
+print(d.get_device())
