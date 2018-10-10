48424805455b8714e9fc04d5cef06d9e3b2b3f16
Kasper Helsted <khelst13@student.aau.dk>
2018-10-10 11:56:29 +0200

    Added takeoff & landing

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import enum
+import time
+from threading import Thread
+
 from inputz import Devices
 from pyparrot.Bebop import Bebop
 
@@ -8,15 +12,76 @@ d = Devices().get_device()
 
 class DroneTest(object):
     def __init__(self):
+        # Dronen skal være en tråd, så de ikke hænger i beregninger til reporter
         self.bebop = Bebop()
+
+        self.max_roll = 100
+        self.max_pitch = 100
+        self.max_yaw = 100
+        self.max_vertical_movement = 100
+
+        self.state = self.State.deed
+        self._tick_rate = 0.1
+        self._movement_vector = {
+            'roll': 0,
+            'pitch': 0,
+            'yaw': 0,
+            'vertical_movement': 0,
+        }
+
+        self.thread = Thread(name='Drone', target=self.tick, args=(()))
+
+    class State(enum.Enum):
+        deed = 'deed'
+        stand_still = 'stand_still'
+        takeoff = 'takeoff'
+        flying = 'flying'
+        landing = 'landing'
+
+    def tick(self):
+        while True:
+            print("TICK")
+            start_time = time.time()
+
+            if self.state == self.State.flying:
+                self.bebop.fly_direct(**self._movement_vector, duration=self._tick_rate)
+
+            sleep_time = self._tick_rate - (time.time() - start_time)
+
+            # Handle too slow calculations
+            if sleep_time > 0:
+                time.sleep(sleep_time)
+            else:
+                print("ERROR TICK TO LONK")
+
+    def start(self):
+        success = self.bebop.connect(10)
+        if success:
+            self.thread.start()
 
     @d.listen(c.binding('takeoff'))
     def take_off(self, args):
-        print('take_off')
+        if self.state == self.State.stand_still and args:
+            self.state = self.State.takeoff
+
+            success = self.bebop.safe_takeoff(5)
+
+            if success:
+                self.state = self.State.flying
+            else:
+                self.state = self.State.stand_still
 
     @d.listen(c.binding('landing'))
     def land(self, args):
-        print('land')
+        if self.state == self.State.flying and args:
+            self.state = self.State.landing
+
+            success = self.bebop.safe_land(5)
+
+            if success:
+                self.state = self.State.stand_still
+            else:
+                self.state = self.State.flying
 
     @d.listen(c.binding('pitch'))
     def pitch(self, args):
@@ -49,4 +114,6 @@ class DroneTest(object):
 
 if __name__ == '__main__':
     drone = DroneTest()
+
+    drone.start()
     d.start(drone)
